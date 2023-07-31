2f4bec4b3e766c5f343be911aed6018b7bc74260
KemalDurukanMRT <kemal.mert@enseedling.com>
2023-07-31 19:45:09 +0200

    serializers

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fp*mks52j2x#46wkv26g9g*5$2gw-4e2g+l*#s+(+v^1qjw$=!'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #my_apps
+    'student_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
